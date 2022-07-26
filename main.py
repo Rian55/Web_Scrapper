f2dc95c89630e5406ea8b3cb241b9329b8537b3a
Rian55 <59274842+Rian55@users.noreply.github.com>
2022-07-26 14:14:20 +0300

    items to excel function added

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ all_items = []
 
 
 class Item(object):
-
+    # TODO:: add predefined values for parameters in init
     def __init__(self, name, url, brand, description, price, currency, image_url, color,
                  quantity, item_type, condition):
         self.name = name
@@ -25,7 +25,8 @@ class Item(object):
 
 
 def initial_request():
-    for i in range(1,100):
+    # TODO:: add proper break condition
+    for i in range(1,10):
         try:
             root_url = 'https://www.bonanzamarket.co.uk/items/search?q[country_to_filter]=GB&q[ship_country]' \
                         '=1&q[tas][186873][]=6627596&q[translate_term]=true&q[page]='+str(i)+'&s=6937&q[' \
@@ -45,7 +46,7 @@ def initial_request():
 def get_item_attributes(link):
     data = requests.get(link).content
     source_code = html.fromstring(data)
-
+    # TODO::add attribute existence checks
     condition = ""
     quantity = ""
     item_type = ""
@@ -79,9 +80,55 @@ def get_item_attributes(link):
             i += 1
 
     all_items.append(Item(name=name, quantity=quantity, item_type=item_type, brand=brand, color=color,
-                          description=description, price=price, currency=currency, image_url=image_url))
+                          description=description, price=price, currency=currency, image_url=image_url,
+                          condition=condition, url=link))
+    print(link)
+
+
+def items_to_xlsx():
+    workbook = xlsxwriter.Workbook('C:/Users/Yenip/OneDrive/Belgeler/Excels/demo.xlsx')
+    worksheet = workbook.add_worksheet()
+    worksheet.set_column('A:A', 30)
+    worksheet.set_column('B:B', 20)
+    worksheet.set_column('C:C', 20)
+    worksheet.set_column('D:D', 40)
+    worksheet.set_column('G:G', 20)
+    worksheet.set_column('J:J', 15)
+    worksheet.set_column('K:K', 15)
+    bold = workbook.add_format({'bold': True})
+
+    worksheet.write('A1', 'Name', bold)
+    worksheet.write('B1', 'Url', bold)
+    worksheet.write('C1', 'Brand', bold)
+    worksheet.write('D1', 'Description', bold)
+    worksheet.write('E1', 'Price', bold)
+    worksheet.write('F1', 'Currency', bold)
+    worksheet.write('G1', 'Image url', bold)
+    worksheet.write('H1', 'Color', bold)
+    worksheet.write('I1', 'Quantity', bold)
+    worksheet.write('J1', 'Item type', bold)
+    worksheet.write('K1', 'Condition', bold)
+
+    for i in range(0, len(all_items)):
+        worksheet.write('A'+str(i+2), all_items[i].name)
+        worksheet.write('B'+str(i+2), all_items[i].url)
+        worksheet.write('C'+str(i+2), all_items[i].brand)
+        worksheet.write('D'+str(i+2), all_items[i].description)
+        worksheet.write('E'+str(i+2), all_items[i].price)
+        worksheet.write('F'+str(i+2), all_items[i].currency)
+        worksheet.write('G'+str(i+2), all_items[i].image_url)
+        worksheet.write('H'+str(i+2), all_items[i].color)
+        worksheet.write('I'+str(i+2), all_items[i].quantity)
+        worksheet.write('J'+str(i+2), all_items[i].item_type)
+        worksheet.write('K'+str(i+2), all_items[i].condition)
+
+    workbook.close()
 
 
 initial_request()
 for item in item_links:
     get_item_attributes(item)
+items_to_xlsx()
+
+
+
